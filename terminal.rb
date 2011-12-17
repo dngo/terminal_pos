ceb1aa85ca41c1e32aa6c913670a9d06a79bb232
David Ngo <davngo@gmail.com>
2011-12-16 20:59:12 -0500

    finishing up

--- terminal.rb
+++ terminal.rb
@@ -1,23 +1,29 @@
-require 'rubygems'
-require 'ruby-debug'
-
 class Terminal
   attr_accessor :item_list, :cart
 
   def initialize
-    @item_list = {}
-    @cart = []
+    @item_list, @cart = {}, []
   end
 
   def set_pricing(item)
-    @item_list[item.name] = item
+    item_list[item.name] = item
   end
 
   def scan(item_name)
-    @cart << @item_list[item_name]
+    if item_list[item_name]
+      cart << item_list[item_name] 
+    else
+      raise ArgumentError, "Item could not be found" 
+    end
   end
 
   def total
+    total_price = 0
+    cart.uniq.each do |item|
+      quantity = cart.count(item)
+      total_price += item.total(quantity)
+    end
+    total_price
   end
 end
 
@@ -25,10 +31,14 @@ class Item
   attr_accessor :name, :price, :bulk_quantity, :bulk_price
 
   def initialize(args)
-    @name = args[:name]
-    @price = args[:price]
-    @bulk_quantity = args[:bulk_quantity]
-    @bulk_price = args[:bulk_price]
+    @name, @price, @bulk_quantity, @bulk_price = args[:name], args[:price], args[:bulk_quantity], args[:bulk_price]
+  end
+
+  def total(quantity)
+    return price * quantity if bulk_price.nil? || quantity < bulk_quantity
+    item_total = 0
+    item_total += quantity/bulk_quantity * bulk_price
+    item_total += quantity.remainder(bulk_quantity) * price
   end
 
 end
--- test.rb
+++ test.rb
@@ -7,28 +7,42 @@ describe Item do
 
   describe "#name" do
     it "should correctly return the name" do
+      @item.name.should eql("A")
     end
   end
 
   describe "#price" do
     it "should correctly return the price" do
+      @item.price.should eql(2)
     end
   end
 
   describe "#bulk_quantity" do
     it "should correctly return the bulk_quantity" do
+      @item.bulk_quantity.should eql(4)
+      @item = Item.new(:name => "A", :price => 2)
+      @item.bulk_quantity.should be_nil
     end
   end
 
   describe "#bulk_price" do
     it "should correctly return the bulk_price" do
+      @item.bulk_price.should eql(7)
+      @item = Item.new(:name => "A", :price => 2)
+      @item.bulk_price.should be_nil
+    end
+  end
+
+  describe "#total" do
+    it "should correctly return the total_price" do
+      @item.total(10).should eql(18)
     end
   end
 
 end
 
 describe Terminal do
-   before(:all) do
+   before(:each) do
     @terminal = Terminal.new
     @terminal.set_pricing Item.new(:name => "A", :price => 2, :bulk_quantity => 4, :bulk_price => 7)
     @terminal.set_pricing Item.new(:name => "B", :price => 12)
@@ -54,14 +68,45 @@ describe Terminal do
       @terminal.cart.size.should eql(1)
       @terminal.cart.first.should eql(@terminal.item_list["A"])
     end
+
+    it "should not scan items that are not on the item list" do
+      @terminal.cart.empty?.should eql(true)
+      lambda { @terminal.scan("Q")}.should raise_error(ArgumentError, "Item could not be found")
+      @terminal.cart.empty?.should eql(true)
+    end
   end
 
   describe "#total" do
-    it "should correctly determine the total" do
+    it "should Scan these items in this order: ABCDABAA; Verify the total price is $32.40" do
       @terminal.scan("A")
-      @terminal.cart.size.should eql(1)
-      @terminal.cart.first.should eql(@terminal.item_list["A"])
+      @terminal.scan("B")
+      @terminal.scan("C")
+      @terminal.scan("D")
+      @terminal.scan("A")
+      @terminal.scan("B")
+      @terminal.scan("A")
+      @terminal.scan("A")
+      @terminal.cart.size.should eql(8)
+      @terminal.total.should eql(32.40)
+    end
+
+    it "should Scan these items in this order: CCCCCCC; Verify the total price is $7.25" do
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.scan("C")
+      @terminal.total.should eql(7.25)
+    end
+
+    it "should Scan these items in this order: ABCD; Verify the total price is $15.40" do
+      @terminal.scan("A")
+      @terminal.scan("B")
+      @terminal.scan("C")
+      @terminal.scan("D")
+      @terminal.total.should eql(15.40)
     end
   end
 end
-
